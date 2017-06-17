3dae2d9fa78d6ba45d1e59eed15642607f84c54e
Anton Yaskevich <anton.yaskevich.mail@gmail.com>
2017-06-17 16:03:07 +0300

    feat(deploy): add prod config for heroku env

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,11 @@ const config = require('../config');
 
 const host = config.get('db:host');
 const port = config.get('db:port');
-const dbname = config.get('db:database')
+const dbname = config.get('db:database');
+const user = config.get('db:user');
+const password = config.get('db:password');
+const prod = config.get('prod');
 
-mongoose.connect(`mongodb://${host}:${port}/${dbname}`);
+mongoose.connect(`mongodb://${prod ? `${user}:${password}@` : ''}${host}:${port}/${dbname}`);
 
 module.exports = mongoose;
