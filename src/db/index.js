619b5999ecee60d023ae2bfb987753c4cc44c47f
Anton Yaskevich <anton.yaskevich.mail@gmail.com>
2017-06-17 16:44:11 +0300

    fix(db): remove auth

--- src/db/index.js
+++ src/db/index.js
@@ -4,17 +4,7 @@ const config = require('../config');
 const host = config.get('db:host');
 const port = config.get('db:port');
 const dbname = config.get('db:database');
-const user = config.get('db:user');
-const pass = config.get('db:password');
-const prod = config.get('prod');
 
-const opt = {
-    db: {
-        user,
-        pass,
-    }
-}
-
-mongoose.connect(`mongodb://${host}:${port}/${dbname}?authSource=admin`, prod ? opt : {});
+mongoose.connect(`mongodb://${host}:${port}/${dbname}`);
 
 module.exports = mongoose;
