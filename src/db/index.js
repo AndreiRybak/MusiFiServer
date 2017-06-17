6f5bc73b3b10cc3596a702ebeda486d531fdd054
Anton Yaskevich <anton.yaskevich.mail@gmail.com>
2017-06-17 16:55:26 +0300

    fix(db): add auth for mLab

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,17 @@ const config = require('../config');
 const host = config.get('db:host');
 const port = config.get('db:port');
 const dbname = config.get('db:database');
+const user = config.get('db:user');
+const pass = config.get('db:password');
+const prod = config.get('prod');
 
-mongoose.connect(`mongodb://${host}:${port}/${dbname}`);
+const opt = {
+    db: {
+        user,
+        pass,
+    }
+}
+
+mongoose.connect(`mongodb://${host}:${port}/${dbname}`, prod ? opt : {});
 
 module.exports = mongoose;
